309050eb85d6b13a54cc1a268cfab40402ac00cf
Cristian Hornedo <officialcristianl@gmail.com>
2020-09-15 13:40:05 -0400

    Added package with dependencies, server.js, db directory with schema and seed, config directory with a connection and orm being built now

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,14 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Hamburger", false);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Cheeseburger", false);
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Big Mac", false);
